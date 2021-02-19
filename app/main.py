51b119e9a95a49c2dc9121a985428ee3bf998aaa
Yamane <candoanything1031@gmail.com>
2021-02-19 17:40:31 +0900

    自動デプロイのテスト

--- app/main.py
+++ app/main.py
@@ -56,6 +56,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
+    print("Auto Deploy Test")
     return users
 
 
