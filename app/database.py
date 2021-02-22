2f0cda4d972c19367d30cd24b1c3dc4732f5ed27
Yamane <candoanything1031@gmail.com>
2021-02-22 13:47:13 +0900

    URL変更

--- app/database.py
+++ app/database.py
@@ -17,13 +17,25 @@ from sqlalchemy.orm import sessionmaker
 #                     "tori-304112:asia-northeast1:tori-db")  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
 #             }
 #         )
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://%s:%s@%s/%s?charset=utf8' % (
+
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://%s:%s@%s/%s?charset=utf8' % (
+#     "tori",
+#     "Yamanekoubou1031",
+#     # "172.24.48.3:3306"
+#     # "tori-304112:asia-northeast1:tori-db"
+#     "172.24.48.3:3306",
+#     # "127.0.0.1:3306"
+#     "test_database",
+#     # "unix_socket=/cloudsql/tori-304112:asia-northeast1:tori-db"
+# )
+
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://{0}:{1}@{2}/{3}?charset=utf8'.format(
     "tori",
     "Yamanekoubou1031",
     # "172.24.48.3:3306"
     # "tori-304112:asia-northeast1:tori-db"
-    # "172.24.48.3:3306",
-    "127.0.0.1:3306"
+    "172.24.48.3:3306",
+    # "127.0.0.1:3306"
     "test_database",
     # "unix_socket=/cloudsql/tori-304112:asia-northeast1:tori-db"
 )
